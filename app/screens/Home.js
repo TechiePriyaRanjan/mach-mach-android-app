c7f9f15a8bb303645d2a8358103199f9d638940c
PriyaRanjan <hackerswanted2012@gmail.com>
2020-12-26 10:44:33 +0530

    getting image in the dashboard

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, View, Image, Dimensions } from "react-native";
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import axios from 'axios';
 import styled from 'styled-components/native';
 import Avatar from '../components/Avatar';
+
+const dimensions = Dimensions.get('window');
+const imageHeight = Math.round(dimensions.width * 9 / 16);
+const imageWidth = dimensions.width;
+
 export class Home extends Component {
   constructor(props) {
     super(props);
@@ -31,20 +36,31 @@ export class Home extends Component {
             <Container>
               <Header>
                 <Row>
-                  <Avatar source={require("../assets/images/profile-pic.jpg")} />
+                  <Image
+                    style={styles.tinyLogo}
+                    source={{ uri: key.profile_pic }}
+                  />
                   <View style={{ paddingLeft: 10 }}>
-                    <User>Neha Sharma</User>
+                    <User>{key.posted_by}</User>
                     <Row>
-                      <Time>Nov 17, 2020 at 2:05 PM</Time>
+                      <Time>{key.posted_on}</Time>
                     </Row>
-                    <Row><Text style={{ color: 'blue' }}>#{key.category}</Text></Row>
+                    <Row><Text style={{ color: 'blue' }}>{key.category}</Text></Row>
                   </View>
                 </Row>
                 <FeatherIcon name="more-vertical" size={24} color="#52575D"></FeatherIcon>
               </Header>
 
-              <Post>{key.post}</Post>
-
+              {key.type === "Image"
+                ?
+                <Image
+                  style={styles.postImage}
+                  source={{ uri: key.post }}
+                  resizeMode="contain"
+                />
+                :
+                <Post>{key.post}</Post>
+              }
               <Footer>
                 <Separator />
                 <FooterMenu>
@@ -52,10 +68,19 @@ export class Home extends Component {
                     <FeatherIcon name="heart" size={18} attrs={{ fill: 'tomato' }} style={{ marginRight: 10 }} />
                     <Text>{key.total_like} Likes</Text>
                   </Button>
-                  <Button>
-                    <FeatherIcon name="copy" size={18} color="#a1a1a1" style={{ marginRight: 10 }} />
-                    <Text>Copy</Text>
-                  </Button>
+                  {
+                    key.type === "Image"
+                      ?
+                      <Button>
+                        <FeatherIcon name="download" size={18} color="#a1a1a1" style={{ marginRight: 10 }} />
+                        <Text>{key.total_download} </Text>
+                      </Button>
+                      :
+                      <Button>
+                        <FeatherIcon name="copy" size={18} color="#a1a1a1" style={{ marginRight: 10 }} />
+                        <Text>Copy</Text>
+                      </Button>
+                  }
                   <Button>
                     <FeatherIcon name="share-2" size={18} color="#a1a1a1" style={{ marginRight: 10 }} />
                     <Text>Share</Text>
@@ -115,7 +140,19 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: "HelveticaNeue",
     color: "#52575D"
-  }
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    borderRadius: 4
+  },
+  postImage: {
+    marginTop: 9,
+    // width: '100%',
+    // height: 300,
+    height: imageHeight,
+    width: imageWidth,
+  },
 });
 
 const Container = styled.View`
